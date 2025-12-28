373764a1dd86482702166a47882eb4d1fb7bc675
v-romanyuk <v.romanyuk@softonix.org>
2025-12-28 13:48:40 +0200

    changed app name

--- lib/core/extensions/string_case_extension.dart
+++ lib/core/extensions/string_case_extension.dart
@@ -1,5 +1,5 @@
 extension StringCaseExtension on String {
   String toCapitalCase({int lastCapitalized = 0}) {
-    return '${this[lastCapitalized].toUpperCase()}${this.substring(lastCapitalized + 1).toLowerCase()}';
+    return '${this[lastCapitalized].toUpperCase()}${substring(lastCapitalized + 1).toLowerCase()}';
   }
 }
